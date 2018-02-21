e0b969c2356e8b1311769ca77ad5c4fc371f5117
Bunker <michaelbunker1@gmail.com>
2018-02-21 01:11:47 -0500

    Convert spacing to tabs

--- src/Util/Script/CheckStyle.php
+++ src/Util/Script/CheckStyle.php
@@ -9,18 +9,18 @@ use Composer\Script\Event;
  */
 class CheckStyle
 {
-    /**
-     * Execute script. function is static to allow call from composer post-install commands via composer.json file.
-     *
-     * @param Event $event
-     *
-     * @return void
-     */
-    public static function execute(Event $event)
-    {
-        $result = null;
-        exec('vendor/bin/phpcs --standard=LeagueLineCS.xml --colors -s --extensions=php src/', $result);
+	/**
+	 * Execute script. function is static to allow call from composer post-install commands via composer.json file.
+	 *
+	 * @param Event $event
+	 *
+	 * @return void
+	 */
+	public static function execute(Event $event)
+	{
+		$result = null;
+		exec('vendor/bin/phpcs --standard=LeagueLineCS.xml --colors -s --extensions=php src/', $result);
 
-        $result > 0 ? $event->getIO()->write($result) : $event->getIO()->write('<info>No check style errors.</info>' . PHP_EOL);
-    }
+		$result > 0 ? $event->getIO()->write($result) : $event->getIO()->write('<info>No check style errors.</info>' . PHP_EOL);
+	}
 }
