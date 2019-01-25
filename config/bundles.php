485b68dae19125dc8ffbe90738b5c91b4f28c52c
Bunker <michaelbunker1@gmail.com>
2019-01-24 21:06:05 -0800

    Update packages and add webpack + vueJS to project

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Emoe\GuzzleBundle\EmoeGuzzleBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
